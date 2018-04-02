a497cbcaf023e0583c13b12c32122d2ad39b5d61
pcASUS_grey\esnho <esnhoesnho@gmail.com>
2018-04-02 17:50:09 +0200

    added other tests

--- Spreading/Assets/Scripts/Editor/Tests/LinearSpreadTests.cs
+++ Spreading/Assets/Scripts/Editor/Tests/LinearSpreadTests.cs
@@ -23,6 +23,26 @@ public class LinearSpreadTests
     }
 
     [Test]
+    public void LinearSpreadIncreaseSize()
+    {
+        int startSize = 5;
+        int newSize = 8;
+        LinearSpread lSpread = new LinearSpread(startSize, 1f, 0f);
+        lSpread.size = newSize;
+        Assert.AreEqual(newSize, lSpread.size);
+    }
+
+    [Test]
+    public void LinearSpreadDiminishSize()
+    {
+        int startSize = 5;
+        int newSize = 2;
+        LinearSpread lSpread = new LinearSpread(startSize, 1f, 0f);
+        lSpread.size = newSize;
+        Assert.AreEqual(newSize, lSpread.size);
+    }
+
+    [Test]
     public void LinearSpreadMinValue()
     {
         LinearSpread lSpread = new LinearSpread(5, 1f, 0f);
@@ -36,12 +56,20 @@ public class LinearSpreadTests
         Assert.AreEqual(0.5f, lSpread.value[lSpread.size - 1]);
     }
 
+    [Test]
+    public void LinearSpreadCenterValue()
+    {
+        float centerVal = 1.5f;
+        LinearSpread lSpread = new LinearSpread(5, 15f, centerVal);
+        Assert.AreEqual(centerVal, lSpread.value[2]);
+    }
+
     // A UnityTest behaves like a coroutine in PlayMode
     // and allows you to yield null to skip a frame in EditMode
-    [UnityTest]
+    /*[UnityTest]
 	public IEnumerator NewEditModeTestWithEnumeratorPasses() {
         // Use the Assert class to test conditions.
         // yield to skip a frame
         yield return null;
-	}
+	}*/
 }
